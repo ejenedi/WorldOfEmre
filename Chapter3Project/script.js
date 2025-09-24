452b2780c0bcfe454832236529c8fb9d98e5ce46
ejenedi <emrejenedi@gmail.com>
2025-09-23 22:45:42 -0500

    Add files via upload

---
+++ Chapter3Project/script.js
@@ -0,0 +1,33 @@
+/*
+  Author: Emre Jenedi
+  Date: 09/23/2025
+  File: script.js
+*/
+
+// Variable for full name
+let fullName = "Emre Jenedi";
+console.log("Full Name:", fullName);
+
+// Variable for desired annual salary
+let desiredSalary = 100000;
+console.log("Desired Salary:", desiredSalary);
+
+// Variable for veteran status
+let isVeteran = false;
+console.log("Veteran Status:", isVeteran);
+
+// Array of three friends' names
+let friends = ["John", "Jack", "Layla"];
+console.log("Friends:", friends);
+
+// Array of three friends' desired salaries
+let friendsSalaries = [80000, 90000, 120000];
+console.log("Friends' Salaries:", friendsSalaries);
+
+// Object for another friend
+let anotherFriend = {
+  firstName: "Jess",
+  lastName: "Smith",
+  desiredSalary: 150000
+};
+console.log("Another Friend:", anotherFriend);
